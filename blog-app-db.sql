7f4a0c2aef320288f0205c4f9482f825788201f1
Kayla Webb <kaylarwebb@gmail.com>
2017-11-20 17:28:10 -0500

    script created the tables

--- blog-app-db.sql
+++ blog-app-db.sql
@@ -1,56 +1,52 @@
--- //Posts - post (text), post_id, author(user), title, content, timestamp, 
--- zero or more tags (null)
--- //Comments - author(user), comment (text), comment_id, post(text), post_id
--- //Users - first_name (text), last_name(text) ,email addresses (required)
--- screen name (required)
--- //Tags - tag(text), tag_id
-
 CREATE TABLE users (
    id SERIAL PRIMARY KEY,
    first_name TEXT,
    last_name TEXT,
    email_address TEXT NOT NULL,
    screen_name TEXT NOT NULL
-)
+);
 
 CREATE TABLE post (
    id SERIAL PRIMARY KEY,
    title TEXT,
    content TEXT,
    user_id INT REFERENCES users ON DELETE RESTRICT
-)
+);
 
 CREATE TABLE comment (
     id SERIAL PRIMARY KEY,
     post_id INT REFERENCES post ON DELETE RESTRICT NOT NULL,
     user_id INT REFERENCES users ON DELETE RESTRICT NOT NULL,
     contents TEXT 
-)
+);
 
 CREATE TABLE tag (
     id SERIAL PRIMARY KEY,
     tag TEXT NOT NULL
-)
+);
 
-INSERT INTO users
-(first_name, last_name, email_address, screen_name) VALUES 
-('Kayla', 'Webb', 'kaylarwebb@gmail.com', 'kwebb91'),
-('Bronson', 'Akimoto', 'hawaii@hawaii.com', 'bronson1'),
-('John', 'Doe', 'jdoe@john.com', 'jdoe10'),
-('Sally', 'Student', 'sstudent@sally.com', 'sstudent19')
+INSERT INTO users (id, first_name, last_name, email_address, screen_name)] VALUES 
+    ('Kayla', 'Webb', 'kaylarwebb@gmail.com', 'kwebb91') 
+    ('Bronson', 'Akimoto', 'hawaii@hawaii.com', 'bronson1') 
+    ('John', 'Doe', 'jdoe@john.com', 'jdoe10') 
+    ('Sally', 'Student', 'sstudent@sally.com', 'sstudent19'), RETURNING id; 
 
-INSERT INTO post
-(title, content, tag, user_id) VALUES 
-('The best blog', 'Current events', 'The best blog post ever'),
-('Blogging', 'Energy', 'Hello world'),
-('Foo bar', 'Foreign affairs', 'Bizz bang'),
-('Fizz buzz', 'mental health', 'Buzz buzz fizz'),
+INSERT INTO post (id, title, content, tag, user_id) VALUES 
+    ('The best blog', 'Current events', 'The best blog post ever') 
+    ('Blogging', 'Energy', 'Hello world')
+    ('Foo bar', 'Foreign affairs', 'Bizz bang') 
+    ('Fizz buzz', 'mental health', 'Buzz buzz fizz') RETURNING id;
+
+INSERT INTO tag (id, tag) VALUES
+    ('current events') 
+    ('energy') 
+    ('foreign affairs')
+    ('mental health') RETURNING id; 
 
 
-INSERT INTO comment
-(post_id, user_id, contents) VALUES
-('worst blog post ever'),
-('i love this post!')
-('interesting perspective')
-('i hate this so much')
+INSERT INTO comment (id, post_id, user_id, contents) VALUES
+    ('worst blog post ever') 
+    ('i love this post!')
+    ('interesting perspective') 
+    ('i hate this so much') RETURNING id; 
 
